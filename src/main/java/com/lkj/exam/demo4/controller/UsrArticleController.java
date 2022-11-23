2c915e6507b0b6d856f9fbf3b96ee9999c02008b
Leekyujin <kyujinlee82@gmail.com>
2022-11-23 17:52:31 +0900

    게시물 수정, 삭제, 상세보기 구현

--- src/main/java/com/lkj/exam/demo4/controller/UsrArticleController.java
+++ src/main/java/com/lkj/exam/demo4/controller/UsrArticleController.java
@@ -20,14 +20,44 @@ public class UsrArticleController {
 		articles = new ArrayList<>();
 	}
 
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+
+		return null;
+	}
+	
+	public Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
 		Article article = new Article(id, title, body);
 
 		articles.add(article);
 		lastArticleId = id;
+		lastArticleId= id;
+
+		return article;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
 
 		return article;
 	}
@@ -37,6 +67,46 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		deleteArticle(id);
+
+		return id + "번 게시물을 삭제했습니다.";
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		modifyArticle(id, title, body);
+
+		return id + "번 게시물이 수정되었습니다.";
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleDetail(int id) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		return article;
 	}
 	
 }
