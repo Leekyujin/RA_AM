4ebb2151c68d71225b780b550fb99809db8c88b3
Leekyujin <kyujinlee82@gmail.com>
2022-11-24 16:44:58 +0900

    로그인 기능 구현

--- src/main/java/com/lkj/exam/demo4/controller/UsrMemberController.java
+++ src/main/java/com/lkj/exam/demo4/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.lkj.exam.demo4.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,4 +52,48 @@ public class UsrMemberController {
 
 		return ResultData.newData(joinRd, member);
 	}
+	
+	@RequestMapping("usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(HttpSession httpsession, String loginId, String loginPw) {
+		
+		boolean isLogined = false;
+
+		if (httpsession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+
+		if (isLogined) {
+			return ResultData.from("F-5", "이미 로그인되어 있습니다.");
+		}
+
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "아이디를 입력해주세요.");
+		}
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "비밀번호를 입력해주세요.");
+		}
+
+		Member member = memberService.getMemberByLoginId(loginId);
+
+		if (member == null) {
+			return ResultData.from("F-3", "아이디를 잘못 입력했습니다.");
+		}
+
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+		}
+		
+		httpsession.setAttribute("loginedMemberId", member.getId());
+
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
+	}
+	
+	@RequestMapping("usr/member/doLogout")
+	@ResponseBody
+	public ResultData doLogout() {
+
+		return ResultData.from("S-1", "로그아웃 되었습니다.");
+	}
+	
 }
